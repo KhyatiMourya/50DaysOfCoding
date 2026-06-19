f6fa12fe49ac37138660c4ed65d443a21e84bd55
Khyati Mourya <khyati1mourya@gmail.com>
2026-06-19 08:41:48 +0530

    Add solution for finding the Nth digit

---
+++ Math-Based/Day9-Nth-Based.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findNthDigit(int n) {
+        long digitLength = 1;
+        long count = 9;
+        long start = 1;
+        while (n > digitLength * count) {
+            n -= digitLength * count;
+            digitLength++;
+            count *= 10;
+            start *= 10;
+        }
+        start += (n - 1) / digitLength;
+        String num = String.valueOf(start);
+        return num.charAt((int)((n - 1) % digitLength)) - '0';
+    }
+}
